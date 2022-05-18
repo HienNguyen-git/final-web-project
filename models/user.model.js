e0ad01cbb477ce5044411a11c5ecdfee19db1f0b
haidang2001 <nchdang16012001@gmail.com>
2022-05-18 15:02:50 +0700

    OTP

---
+++ models/user.model.js
@@ -0,0 +1,16 @@
+const connect = require('../database/db');
+
+const handlePostOTP = (otp,expired) => new Promise((resolve, reject) => {
+    const sql = "insert into otp(otp,expired) values(?,?)"
+    const value = [otp,expired];
+    connect.query(sql, value, (err) => {
+        if (err) reject(false)
+        else{
+            resolve(true);
+        }
+    })
+})
+
+module.exports = {
+    handlePostOTP,
+}
