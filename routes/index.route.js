78e053fbe533462c1a466275219a8d79083590dc
HienNguyen-git <nguyentronghien.contact@gmail.com>
2022-05-19 00:08:19 +0700

    Phone card template

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const { handlePhoneCard } = require('../controllers/phone_card.controller');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/phone-card', handlePhoneCard)
 
 module.exports = router;
