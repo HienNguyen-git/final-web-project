3b099c149a40b97318b894437dae1b8bdb28e009
Duc <hdrake1302@gmail.com>
2022-05-31 16:28:41 +0700

    updaate sql

---
+++ config/database (3).sql
@@ -0,0 +1,342 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 31, 2022 at 11:27 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bill`
+--
+
+CREATE TABLE `bill` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `provider_number` int(11) NOT NULL,
+  `code` varchar(1000) NOT NULL,
+  `price` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `credit_card`
+--
+
+CREATE TABLE `credit_card` (
+  `id` int(11) NOT NULL,
+  `card_number` int(11) NOT NULL,
+  `expire_date` date NOT NULL,
+  `cvv` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `credit_card`
+--
+
+INSERT INTO `credit_card` (`id`, `card_number`, `expire_date`, `cvv`) VALUES
+(1, 111111, '2022-10-10', 411),
+(2, 222222, '2022-11-11', 443),
+(3, 333333, '2022-12-12', 577);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deposit`
+--
+
+CREATE TABLE `deposit` (
+  `id` int(11) NOT NULL,
+  `phone_sender` varchar(255) NOT NULL,
+  `phone_receiver` varchar(255) NOT NULL,
+  `value` int(11) NOT NULL,
+  `fee` int(11) NOT NULL,
+  `feeperson` varchar(255) NOT NULL,
+  `note` varchar(1000) NOT NULL,
+  `status` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `network_provider`
+--
+
+CREATE TABLE `network_provider` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `provider_number` int(11) NOT NULL,
+  `fee` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `network_provider`
+--
+
+INSERT INTO `network_provider` (`id`, `name`, `provider_number`, `fee`) VALUES
+(1, 'Viettel', 11111, 0),
+(2, 'Mobifone ', 22222, 0),
+(3, 'Vinaphone ', 33333, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `otp`
+--
+
+CREATE TABLE `otp` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `otpcode` varchar(255) NOT NULL,
+  `expired` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `otp`
+--
+
+INSERT INTO `otp` (`id`, `email`, `otpcode`, `expired`) VALUES
+(135, 'nghaing2001@gmail.com', '555715', '2022-05-31 08:59:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recharge`
+--
+
+CREATE TABLE `recharge` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `card_number` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
+  `value` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `recharge`
+--
+
+INSERT INTO `recharge` (`id`, `username`, `card_number`, `date`, `value`) VALUES
+(35, '5388754202', '111111', '2022-05-31 16:11:20', 10000000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT -1,
+  `last_modified` datetime NOT NULL DEFAULT current_timestamp(),
+  `login_attempts` int(11) NOT NULL DEFAULT 0,
+  `abnormal` int(11) NOT NULL,
+  `login_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `status`, `last_modified`, `login_attempts`, `abnormal`, `login_date`) VALUES
+(1, 'admin', '$2a$12$G0bqH0PVQmBumlSh.8N8PumaxpU0VV95isT/ZtZ.IaMea/kKnJ4Ui', 1, '2022-05-18 12:42:54', 4, 0, '2022-05-30 13:43:21'),
+(9, '5388754202', '$2b$10$4wacp0dnE9YhAeWOlKUz4eXJNYPTfcR9KhrgoV.k.ETaY8toOOkA6', 1, '2022-05-31 15:53:21', 0, 0, '2022-05-31 15:50:26'),
+(10, '3961023332', '$2b$10$sIhcBK.baGJ8BmEkUGahZuOww0OYxsMDYb2pwb9AXl8TQ5K1MAPoK', 2, '2022-05-31 16:02:56', 0, 0, '2022-05-31 16:00:49'),
+(11, '2544562999', '$2b$10$UMCbOA.yTOqnedYXTmFlC.d1S6yRau5EIK2t4HJTZvO3GSt8tuY4.', 1, '2022-05-31 16:08:40', 6, 0, '2022-05-31 16:06:01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_detail`
+--
+
+CREATE TABLE `user_detail` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `address` varchar(1000) NOT NULL,
+  `font_cmnd` varchar(255) NOT NULL,
+  `back_cmnd` varchar(255) NOT NULL,
+  `total_value` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_detail`
+--
+
+INSERT INTO `user_detail` (`id`, `username`, `phone`, `email`, `name`, `date_of_birth`, `address`, `font_cmnd`, `back_cmnd`, `total_value`) VALUES
+(6, '5388754202', '0934333333', 'nghaing2001@gmail.com', 'Đức Đăng', '2022-05-05', 'Bình Điền', '1653987023239--dwayne-the-rock-.jpg', '1653987023239--MAS_6984.jpg', 9947500),
+(7, '3961023332', '0909922333', 'asd@gmail.com', 'Đăng Hiển', '2022-05-18', 'Biên Hòa', '1653987646693--MAS_6984.jpg', '1653987646693--photo-1597223557154-721c1cecc4b0.jpg', 0),
+(8, '2544562999', '0999329827', 'hdrake1302@gmail.com', 'Đức Hoàng', '2022-05-25', 'address', '1653988095796--photo-1597223557154-721c1cecc4b0.jpg', '1653988095796--288260.png', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `withdraw`
+--
+
+CREATE TABLE `withdraw` (
+  `id` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `card_number` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `value` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `fee` int(11) NOT NULL,
+  `note` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `withdraw`
+--
+
+INSERT INTO `withdraw` (`id`, `username`, `card_number`, `date`, `value`, `status`, `fee`, `note`) VALUES
+(28, '5388754202', 111111, '2022-05-31 16:24:41', 50000, 1, 2500, 'asd'),
+(29, '5388754202', 111111, '2022-05-31 16:24:51', 6000000, 0, 300000, 'asd');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `credit_card`
+--
+ALTER TABLE `credit_card`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `deposit`
+--
+ALTER TABLE `deposit`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `network_provider`
+--
+ALTER TABLE `network_provider`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `otp`
+--
+ALTER TABLE `otp`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `recharge`
+--
+ALTER TABLE `recharge`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_detail`
+--
+ALTER TABLE `user_detail`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `withdraw`
+--
+ALTER TABLE `withdraw`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bill`
+--
+ALTER TABLE `bill`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `credit_card`
+--
+ALTER TABLE `credit_card`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `deposit`
+--
+ALTER TABLE `deposit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `network_provider`
+--
+ALTER TABLE `network_provider`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `otp`
+--
+ALTER TABLE `otp`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=136;
+
+--
+-- AUTO_INCREMENT for table `recharge`
+--
+ALTER TABLE `recharge`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `user_detail`
+--
+ALTER TABLE `user_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `withdraw`
+--
+ALTER TABLE `withdraw`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
